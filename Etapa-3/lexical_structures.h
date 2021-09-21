b426ad19ad14bc9c0819a29b015babd7460bdf80
GuiOliveira98 <guil.oliveira98@gmail.com>
2021-09-21 06:05:35 -0300

    E3: Add experimental commentaries to close issue #9

--- Etapa-3/lexical_structures.h
+++ Etapa-3/lexical_structures.h
@@ -5,27 +5,37 @@
 
 #include <stdbool.h> 
 
+// 2.1: tipo do token
 typedef enum TokenType {
-	SPECIAL_CHAR, //string
-	COMPOSITE_OPERATOR, //string
-	IDENTIFIER, //string
-	LITERAL_INT, //integer
-	LITERAL_FLOAT, //floating_point
-	LITERAL_CHAR, //character
-	LITERAL_BOOL, //boolean
-	LITERAL_STRING, //string
+	SPECIAL_CHAR, 		// caracteres especiais,
+	COMPOSITE_OPERATOR, // operadores compostos,
+	IDENTIFIER, 		// identificadores e
+
+	// literais...
+	// 2.1: Uma forma de implementar o valor do token para literais é utilizar dois campos:
+	// um tipo de literal...
+	LITERAL_INT, 		
+	LITERAL_FLOAT, 		
+	LITERAL_CHAR, 		
+	LITERAL_BOOL, 		
+	LITERAL_STRING, 	
 } token_type_t;
 
+// e o valor associado a ele através de uma construção union da linguagem C.
+
+// 2.1: Os tokens de valores literais devem ter um tratamento especial, pois o valor do token
+// deve ser convertido para o tipo apropriado (...
 typedef union TokenValue {
-	char character;
-	int integer;
-	float floating_point;
-	bool boolean;
-	char* string;
+	int integer;			// inteiro int
+	char character;			// caractere char	
+	float floating_point;	// ponto-flutuante float
+	bool boolean;			// booleano bool
+	char* string;			// ou cadeia de caracteres char*)
 } token_value_t;
 
+// 2.1: O tipo do valor_lexico (e por consequência o valor que será retido) deve ser uma estrutura de dados que contém os seguintes campos:
 typedef struct ValorLexico {
-	int line_number;
-	token_type_t token_type;
-	token_value_t token_value;
+	int line_number;			// 1. número da linha onde apareceu o lexema;
+	token_type_t token_type;	// 2. tipo do token (caracteres especiais, operadores compostos, identificadores e literais);
+	token_value_t token_value;	// 3. valor do token.
 } valor_lexico_t;
