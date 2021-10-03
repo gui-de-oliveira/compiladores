0641d33f1518609f1839054354857ae42cab4840
Jean Pierre Darricarrere <jpierred@gmail.com>
2021-10-03 16:11:21 -0300

    Adds extra tests and fixes precedence

--- Etapa-3/aslTests.js
+++ Etapa-3/aslTests.js
@@ -18,7 +18,7 @@ function listOfSymbolsToDictionaryOfSymbols(listOfSymbols) {
   return listOfSymbols.reduce((previousValue, currentSymbol) => {
     if (previousValue[currentSymbol] === undefined) {
       const totalKeys = Object.keys(previousValue).length;
-      const letter = String.fromCharCode(65 + totalKeys);
+      const letter = String.fromCharCode((totalKeys < 26 ? 65 : 71) + totalKeys); // Skip the 6 ascii characters between upper and lower-case.
       previousValue[currentSymbol] = letter;
     }
 
@@ -766,6 +766,463 @@ async function test() {
     `
   );
 
+  //w65
+  await testInput(
+    `
+    int f(){
+      int a;
+      int x;
+      int b;
+      int c;
+      int y;
+      int d;
+      int e;
+      int f;
+      int z;
+      int g;
+      int h;
+      int i;
+      int w;
+      int j;
+      int k;
+      int l;
+      x = a - b * c;
+      y = d / e + f;
+      z = (g - h) / i;
+      w = j * (k + l);
+   }
+    `,
+    `
+    A, B
+    B, C
+    B, D
+    D, E
+    D, F
+    F, G
+    F, H
+    B, I
+    I, J
+    I, K
+    K, L
+    L, M
+    L, N
+    K, O
+    I, P
+    P, Q
+    P, R
+    R, S
+    S, T
+    S, U
+    R, V
+    P, W
+    W, X
+    W, Y
+    Y, Z
+    Y, a
+    a, b
+    a, c
+    A [label="f"];
+    B [label="="];
+    C [label="x"];
+    D [label="-"];
+    E [label="a"];
+    F [label="*"];
+    G [label="b"];
+    H [label="c"];
+    I [label="="];
+    J [label="y"];
+    K [label="+"];
+    L [label="/"];
+    M [label="d"];
+    N [label="e"];
+    O [label="f"];
+    P [label="="];
+    Q [label="z"];
+    R [label="/"];
+    S [label="-"];
+    T [label="g"];
+    U [label="h"];
+    V [label="i"];
+    W [label="="];
+    X [label="w"];
+    Y [label="*"];
+    Z [label="j"];
+    a [label="+"];
+    b [label="k"];
+    c [label="l"];
+    `
+  );
+
+  //w66
+  await testInput(
+    `
+    int f(){
+      int x;
+      int a;
+      int b;
+      int c;
+      int d;
+      int e;
+      int y;
+      int f;
+      int g;
+      int h;
+      int i;
+      int j;
+      int z;
+      int k;
+      int l;
+      int m;
+      int n;
+      int o;
+      x = a + b + c - d - e;
+      y = f * g * h / i / j;
+      z = k * l * m + n + o;
+   }
+    `,
+    `
+    A, B
+    B, C
+    B, D
+    D, E
+    E, F
+    F, G
+    G, H
+    G, I
+    F, J
+    E, K
+    D, L
+    B, M
+    M, N
+    M, O
+    O, P
+    P, Q
+    Q, R
+    R, S
+    R, T
+    Q, U
+    P, V
+    O, W
+    M, X
+    X, Y
+    X, Z
+    Z, a
+    a, b
+    b, c
+    c, d
+    c, e
+    b, f
+    a, g
+    Z, h
+    A [label="f"];
+    B [label="="];
+    C [label="x"];
+    D [label="-"];
+    E [label="-"];
+    F [label="+"];
+    G [label="+"];
+    H [label="a"];
+    I [label="b"];
+    J [label="c"];
+    K [label="d"];
+    L [label="e"];
+    M [label="="];
+    N [label="y"];
+    O [label="/"];
+    P [label="/"];
+    Q [label="*"];
+    R [label="*"];
+    S [label="f"];
+    T [label="g"];
+    U [label="h"];
+    V [label="i"];
+    W [label="j"];
+    X [label="="];
+    Y [label="z"];
+    Z [label="+"];
+    a [label="+"];
+    b [label="*"];
+    c [label="*"];
+    d [label="k"];
+    e [label="l"];
+    f [label="m"];
+    g [label="n"];
+    h [label="o"];
+    `
+  );
+
+  //w67
+  await testInput(
+    `
+    int f(){
+      int a;
+      int x;
+      int b;
+      int c;
+      int y;
+      int d;
+      int e;
+      int f;
+      int z;
+      int g;
+      int h;
+      int i;
+      int w;
+      int j;
+      int k;
+      int l;
+      x = a + b * c;
+      y = d / e - f;
+      z = (g + h) * i;
+      w = j / (k - l);
+   }
+    `,
+    `
+    A, B
+    B, C
+    B, D
+    D, E
+    D, F
+    F, G
+    F, H
+    B, I
+    I, J
+    I, K
+    K, L
+    L, M
+    L, N
+    K, O
+    I, P
+    P, Q
+    P, R
+    R, S
+    S, T
+    S, U
+    R, V
+    P, W
+    W, X
+    W, Y
+    Y, Z
+    Y, a
+    a, b
+    a, c
+    A [label="f"];
+    B [label="="];
+    C [label="x"];
+    D [label="+"];
+    E [label="a"];
+    F [label="*"];
+    G [label="b"];
+    H [label="c"];
+    I [label="="];
+    J [label="y"];
+    K [label="-"];
+    L [label="/"];
+    M [label="d"];
+    N [label="e"];
+    O [label="f"];
+    P [label="="];
+    Q [label="z"];
+    R [label="*"];
+    S [label="+"];
+    T [label="g"];
+    U [label="h"];
+    V [label="i"];
+    W [label="="];
+    X [label="w"];
+    Y [label="/"];
+    Z [label="j"];
+    a [label="-"];
+    b [label="k"];
+    c [label="l"];
+    `
+  );
+
+  //w68
+  await testInput(
+    `
+    int k(int x, int y, int z){}
+    int f(){
+            k(10+11,20+21,30+31);
+    }
+    `,
+    `
+    A, B
+    B, C
+    C, D
+    D, E
+    D, F
+    D, G
+    G, H
+    G, I
+    G, J
+    J, K
+    J, L
+    A [label="k"];
+    B [label="f"];
+    C [label="call k"];
+    D [label="+"];
+    E [label="10"];
+    F [label="11"];
+    G [label="+"];
+    H [label="20"];
+    I [label="21"];
+    J [label="+"];
+    K [label="30"];
+    L [label="31"];
+    `
+  );
+
+  //w69
+  await testInput(
+    `
+    int f ()
+    {
+      {
+        {
+          {};
+        };
+      };
+    }
+    `,
+    `
+    A [label="f"];
+    `
+  );
+
+  //w70
+  await testInput(
+    `
+    int f()
+    {
+      int b <= 10;
+      int a <= b;
+    }
+    `,
+    `
+    A, B
+    B, C
+    B, D
+    B, E
+    E, F
+    E, G
+    A [label="f"];
+    B [label="<="];
+    C [label="b"];
+    D [label="10"];
+    E [label="<="];
+    F [label="a"];
+    G [label="b"];
+    `
+  );
+
+  //w71
+  await testInput(
+    `
+    int a[10];
+    int f()
+    {
+      int c;
+      c = 2;
+      a[c+c] = 2;
+    }
+    `,
+    `
+    A, B
+    B, C
+    B, D
+    B, E
+    E, F
+    F, G
+    F, H
+    H, I
+    H, J
+    E, K
+    A [label="f"];
+    B [label="="];
+    C [label="c"];
+    D [label="2"];
+    E [label="="];
+    F [label="[]"];
+    G [label="a"];
+    H [label="+"];
+    I [label="c"];
+    J [label="c"];
+    K [label="2"];
+    `
+  );
+
+  //w72
+  await testInput(
+    `
+    int f()
+    {
+      int a;
+      int b;
+      int c;
+      a = 2*c+a/b;
+    }
+    `,
+    `
+    A, B
+    B, C
+    B, D
+    D, E
+    E, F
+    E, G
+    D, H
+    H, I
+    H, J
+    A [label="f"];
+    B [label="="];
+    C [label="a"];
+    D [label="+"];
+    E [label="*"];
+    F [label="2"];
+    G [label="c"];
+    H [label="/"];
+    I [label="a"];
+    J [label="b"];
+    `
+  );
+
+  //w73
+  await testInput(
+    `
+    int f()
+    {
+      c[5] = a[2*c+k];
+    }
+    
+    `,
+    `
+    A, B
+    B, C
+    C, D
+    C, E
+    B, F
+    F, G
+    F, H
+    H, I
+    I, J
+    I, K
+    H, L
+    A [label="f"];
+    B [label="="];
+    C [label="[]"];
+    D [label="c"];
+    E [label="5"];
+    F [label="[]"];
+    G [label="a"];
+    H [label="+"];
+    I [label="*"];
+    J [label="2"];
+    K [label="c"];
+    L [label="k"];
+    `
+  );
+
+  //w74
   await testInput(
     `
     int c[10];
@@ -801,6 +1258,7 @@ async function test() {
     `
   );
 
+  //w75
   await testInput(
     `
     int f()
@@ -835,6 +1293,7 @@ async function test() {
     `
   );
 
+  //w76
   await testInput(
     `
     int v1;
