a35f5a68712e329b7eb4e34367c49522851d0453
GuiOliveira98 <guil.oliveira98@gmail.com>
2021-10-03 19:32:48 -0300

    E3: Parallelized memory tests

---
+++ Etapa-3/runMemTests.js
@@ -0,0 +1,29 @@
+const util = require("util");
+const exec = util.promisify(require("child_process").exec);
+
+async function testFile(i) {
+  let f_one = `w`;
+  let f_two = i < 10 ? `0` : ``;
+  let f_three = i.toString();
+  let file = `./TestsE3/`.concat(f_one).concat(f_two).concat(f_three);
+  await exec(
+    `valgrind --leak-check=full --error-exitcode=2 ./etapa3 < ${file}`
+  );
+  console.log(`Test ${i} over.`);
+}
+
+async function test() {
+  await exec("make");
+
+  const promises = [];
+  for (let i = 0; i <= 76; i++) {
+    console.log(`Started test ${i}...`);
+    promises.push(testFile(i));
+  }
+
+  Promise.all(promises);
+
+  await exec("make clean");
+}
+
+test();
