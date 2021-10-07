c23774360ea9585efda36c76c2ef67ac5e52535f
Jean Pierre Darricarrere <jpierred@gmail.com>
2021-10-07 16:55:57 -0300

    Refactors TopLevelDef into the AstNode trait

--- Etapa-4/src/ast_node.rs
+++ Etapa-4/src/ast_node.rs
@@ -1,8 +1,29 @@
 use lrpar::NonStreamingLexer;
 use std::ffi::c_void;
+use std::fmt::Debug;
 
-pub trait AstNode {
-    fn print_dependencies(&self, own_address: *const c_void);
+pub trait AstNode: Debug {
+    fn print_dependencies(&self, own_address: *const c_void, ripple: bool);
     fn print_labels(&self, lexer: &dyn NonStreamingLexer<u32>, own_address: *const c_void);
     fn is_tree_member(&self) -> bool;
+    fn set_next(&mut self, new_next: Box<dyn AstNode>);
+    fn append_to_next(&mut self, new_last: Box<dyn AstNode>);
+}
+
+impl AstNode for Box<dyn AstNode> {
+    fn print_dependencies(&self, own_address: *const c_void, ripple: bool) {
+        self.as_ref().print_dependencies(own_address, ripple)
+    }
+    fn print_labels(&self, lexer: &dyn NonStreamingLexer<u32>, own_address: *const c_void) {
+        self.as_ref().print_labels(lexer, own_address)
+    }
+    fn is_tree_member(&self) -> bool {
+        self.as_ref().is_tree_member()
+    }
+    fn set_next(&mut self, new_next: Box<dyn AstNode>) {
+        self.as_mut().set_next(new_next)
+    }
+    fn append_to_next(&mut self, new_last: Box<dyn AstNode>) {
+        self.as_mut().append_to_next(new_last)
+    }
 }
--- Etapa-4/src/auxiliary_structures.rs
+++ Etapa-4/src/auxiliary_structures.rs
@@ -1,4 +1,6 @@
-use super::lexical_structures::{Literal, Parameter, SimpleCommand};
+use super::ast_node::AstNode;
+use super::lexical_structures::{GlobalVarDef, GlobalVecDef, Literal, Parameter, SimpleCommand};
+use anyhow::{bail, Result};
 use lrpar::Span;
 
 #[derive(Debug)]
@@ -23,4 +25,37 @@ pub enum AuxTopDefEnd {
     SingleGlob,
     GlobList(Vec<AuxVarOrVecName>),
     VecAndGlobList(Span, Vec<AuxVarOrVecName>),
+}
+
+pub fn top_level_def_assembler(
+    is_static: bool,
+    var_type: Span,
+    mut var_or_vec: Vec<AuxVarOrVecName>,
+) -> Result<Box<dyn AstNode>> {
+    var_or_vec.reverse();
+
+    let mut last_step: Option<Box<dyn AstNode>> = None;
+    match loop {
+        let next_step = match var_or_vec.pop() {
+            Some(def) => def,
+            None => break last_step,
+        };
+        last_step = {
+            let last_node = match last_step {
+                Some(node) => Some(node as Box<dyn AstNode>),
+                None => None,
+            };
+            Some(match next_step {
+                AuxVarOrVecName::Var(var_name) => {
+                    Box::new(GlobalVarDef::new(is_static, var_type, var_name, last_node))
+                }
+                AuxVarOrVecName::Vec { name, size } => Box::new(GlobalVecDef::new(
+                    is_static, var_type, name, size, last_node,
+                )),
+            })
+        }
+    } {
+        None => bail!("top_level_def_assembler() with empty length var_or_vec"),
+        Some(def) => Ok(def),
+    }
 }
