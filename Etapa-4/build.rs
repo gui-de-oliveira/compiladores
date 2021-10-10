430f4d6ed67468dd6775b3ded70f33709566bd96
Jean Pierre Comerlatto Darricarrere <jpierred@gmail.com>
2021-10-10 12:20:27 -0300

    Fixes comments in build.rs

--- Etapa-4/build.rs
+++ Etapa-4/build.rs
@@ -6,14 +6,10 @@ use cfgrammar::yacc::YaccKind;
 use lrlex::LexerBuilder;
 use lrpar::{CTParserBuilder, RecoveryKind};
 
-//Box<dyn std::error::Error>
+// Build function to compile the lexer and parser, for future use in the src/main.rs program entry point.
 fn main() -> Result<(), Box<dyn std::error::Error>> {
     // First we create the parser, which returns a HashMap of all the tokens used, then we pass
     // that HashMap to the lexer.
-    //
-    // Note that we specify the integer type (u8) we'll use for token IDs (this type *must* be big
-    // enough to fit all IDs in) as well as the input file (which must end in ".y" for lrpar, and
-    // ".l" for lrlex).
     let lex_rule_ids_map = CTParserBuilder::new()
         .yacckind(YaccKind::Grmtools)
         .recoverer(RecoveryKind::None) // Remove to enable the error recovery feature.
