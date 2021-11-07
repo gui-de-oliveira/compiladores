adb46d5e66e63917479955ce00db04d5e388b703
Jean Pierre Comerlatto Darricarrere <jpierred@gmail.com>
2021-11-07 16:47:39 -0300

    Implements NotEqual

--- Etapa-5/src/lexical_structures.rs
+++ Etapa-5/src/lexical_structures.rs
@@ -3759,7 +3759,7 @@ impl Binary {
                             highlight,
                         })
                     }
-                    SymbolType::Bool(_) | SymbolType::Int(_) => {
+                    SymbolType::Bool(_) => {
                         match (
                             left_value.to_int(self.node_id, lexer)?,
                             right_value.to_int(self.node_id, lexer)?,
@@ -3770,6 +3770,105 @@ impl Binary {
                             (_, _) => Ok(SymbolType::Bool(BoolValue::Undefined)),
                         }
                     }
+                    SymbolType::Int(_) => match (left_value, right_value) {
+                        (SymbolType::Int(left_value), SymbolType::Int(right_value)) => match (left_value, right_value) {
+                            bad @ (IntValue::Undefined, _)
+                            | bad @ (_, IntValue::Undefined) => {
+                                Err(CompilerError::IlocErrorUndefinedBehavior(format!(
+                                    "Binary operation NotEqual matched undefined with something else: {:?}",
+                                    bad
+                                )))
+                            }
+                            (IntValue::Temp(register), IntValue::Literal(other_value)) | (IntValue::Literal(other_value), IntValue::Temp(register)) => {
+                                let new_register = code.new_register();
+                                code.push_code(CodeLine::Deliver(Instruction::Unlabeled(Operation::LoadI(
+                                    other_value,
+                                    new_register,
+                                ))));
+                                code.push_code(CodeLine::Deliver(Instruction::Unlabeled(Operation::CmpNE(
+                                    register,
+                                    new_register,
+                                    register,
+                                ))));
+                                Ok(SymbolType::Int(IntValue::Temp(register)))
+                            }
+                            (IntValue::Temp(register), IntValue::Memory(other_register, offset)) | (IntValue::Memory(other_register, offset), IntValue::Temp(register)) => {
+                                let new_register = code.new_register();
+                                code.push_code(CodeLine::Deliver(Instruction::Unlabeled(Operation::LoadAI(
+                                    other_register,
+                                    offset as i32,
+                                    new_register,
+                                ))));
+                                code.push_code(CodeLine::Deliver(Instruction::Unlabeled(Operation::CmpNE(
+                                    register,
+                                    new_register,
+                                    register,
+                                ))));
+                                Ok(SymbolType::Int(IntValue::Temp(register)))
+                            }
+                            (IntValue::Literal(value), IntValue::Memory(mem_register, offset)) | (IntValue::Memory(mem_register, offset), IntValue::Literal(value)) => {
+                                let new_register = code.new_register();
+                                code.push_code(CodeLine::Deliver(Instruction::Unlabeled(Operation::LoadAI(
+                                    mem_register,
+                                    offset as i32,
+                                    new_register,
+                                ))));
+                                let lit_register = code.new_register();
+                                code.push_code(CodeLine::Deliver(Instruction::Unlabeled(Operation::LoadI(
+                                    value,
+                                    lit_register,
+                                ))));
+                                code.push_code(CodeLine::Deliver(Instruction::Unlabeled(Operation::CmpNE(
+                                    new_register,
+                                    lit_register,
+                                    new_register,
+                                ))));
+                                Ok(SymbolType::Int(IntValue::Temp(new_register)))
+                            }
+                            (
+                                IntValue::Literal(left_value),
+                                IntValue::Literal(right_value),
+                            ) => Ok(SymbolType::Bool(BoolValue::Literal(left_value != right_value))),
+                            (
+                                IntValue::Memory(mem_left_register, left_offset),
+                                IntValue::Memory(mem_right_register, right_offset),
+                            ) => {
+                                let left_register = code.new_register();
+                                code.push_code(CodeLine::Deliver(Instruction::Unlabeled(Operation::LoadAI(
+                                    mem_left_register,
+                                    left_offset as i32,
+                                    left_register,
+                                ))));
+                                let right_register = code.new_register();
+                                code.push_code(CodeLine::Deliver(Instruction::Unlabeled(Operation::LoadAI(
+                                    mem_right_register,
+                                    right_offset as i32,
+                                    right_register,
+                                ))));
+                                code.push_code(CodeLine::Deliver(Instruction::Unlabeled(Operation::CmpNE(
+                                    left_register,
+                                    right_register,
+                                    left_register,
+                                ))));
+                                Ok(SymbolType::Int(IntValue::Temp(left_register)))
+                            },
+                            (
+                                IntValue::Temp(left_register),
+                                IntValue::Temp(right_register),
+                            ) => {
+                                code.push_code(CodeLine::Deliver(Instruction::Unlabeled(Operation::CmpNE(
+                                    left_register,
+                                    right_register,
+                                    left_register,
+                                ))));
+                                Ok(SymbolType::Int(IntValue::Temp(left_register)))
+                            },
+                        }
+                        bad => Err(CompilerError::IlocErrorUndefinedBehavior(format!(
+                            "Binary operation NotEqual with unsuported types: {:?}",
+                            bad
+                        ))),
+                    },
                     SymbolType::Float(_) => match (
                         left_value.to_float(self.node_id, lexer)?,
                         right_value.to_float(self.node_id, lexer)?,
